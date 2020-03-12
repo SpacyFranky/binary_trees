566738a1dc3ab0e22984cb2e78173afa73c70fc8
iheb1919 <ihebmejri14@gmail.com>
2020-03-12 18:25:08 +0100

    ff

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,18 +7,23 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int i = 0;
+	int i = 0, j = 0;
 	if (tree == NULL)
 		return (0);
 
 	if (!tree)
 		return (0);
-	if ((tree->left == NULL && tree->right == NULL) ||
-	    (tree->left != NULL && tree->right != NULL))
-		i = binary_tree_is_perfect(tree->left) + binary_tree_is_perfect(tree->right);
-	else
-		i = 1;
-	if (i == 1){
-		return (0);}
-	return (1);
+	if ((tree->left == NULL && tree->right == NULL))
+	{
+		i++;
+		binary_tree_is_perfect(tree->left);
+	}
+	else if ((tree->left != NULL && tree->right != NULL))
+	{
+		j++;
+		binary_tree_is_perfect(tree->right);
+	}
+	if (i == j)
+		return (1);
+	return (0);
 }
