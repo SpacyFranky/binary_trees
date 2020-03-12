83bcdb3520fad87c499e73924b85e438f598d0ba
iheb1919 <ihebmejri14@gmail.com>
2020-03-12 18:50:32 +0100

    fix task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,8 +7,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t i = 0, j = 0;
+
 	if (tree == NULL)
-		return (-1);
-	return (binary_tree_height(tree->left),
-		binary_tree_height(tree->right) + 1);
+		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		i = binary_tree_height(tree->left) + 1;
+		j = binary_tree_height(tree->right) + 1;
+	}
+	if (i < j)
+		return (j);
+	return (i);
 }
