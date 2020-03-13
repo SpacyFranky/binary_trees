f82b3253ccd8732376991ba07b01445a4831fec0
SpacyFranky <bigaziz@outlook.com>
2020-03-13 11:40:59 +0000

    fixing output errors

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,10 +12,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-
-	if (tree == NULL)
-		return (0);
 	if ((tree->left == NULL && tree->right == NULL) ||
 	    (tree->left != NULL && tree->right != NULL))
 		i = binary_tree_is_full(tree->left) + binary_tree_is_full(tree->right);
